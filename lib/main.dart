583251bda424a1ff4f09c3123024bdd386d3c50a
이상현 <firstquarterlee@gmail.com>
2024-08-01 22:48:20 +0900

    Test: http 통신 테스트 성공

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -24,6 +27,15 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key});
 
+  Future fetch() async {
+    var url = Uri.https('gist.githubusercontent.com', '/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/sample.json');
+
+
+    var response = await http.get(url);
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${jsonDecode(utf8.decode(response.bodyBytes))}');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,7 +43,10 @@ class MyHomePage extends StatelessWidget {
         title: Text('title'),
       ),
       body: Center(
-        child: Text('text'),
+        child: ElevatedButton(
+        onPressed: fetch,
+        child: Text('테스트'),
+        ),
       ),
     );
   }
