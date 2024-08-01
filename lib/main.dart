627028bd977ac8c6b328533a4d47687fbd0f1d17
이상현 <firstquarterlee@gmail.com>
2024-08-02 01:35:38 +0900

    Test: json 데이터를 List<Stroe>로 변환

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_mask/model/store.dart';
 import 'package:http/http.dart' as http;
 
 void main() {
@@ -10,7 +11,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,27 +24,43 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Store> stores = [];
 
   Future fetch() async {
     var url = Uri.https('gist.githubusercontent.com', '/junsuk5/bb7485d5f70974deee920b8f0cd1e2f0/raw/063f64d9b343120c2cb01a6555cf9b38761b1d94/sample.json');
 
-
     var response = await http.get(url);
-    print('Response status: ${response.statusCode}');
-    print('Response body: ${jsonDecode(utf8.decode(response.bodyBytes))}');
+
+    final jsonResult = jsonDecode(utf8.decode(response.bodyBytes));
+
+    final jsonStores = jsonResult['stores'];
+
+    stores.clear();
+    jsonStores.forEach((e) {
+      stores.add(Store.fromJson(e));
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('title'),
+        title: Text('마스크 재고 있는 곳 : 0곳'),
       ),
       body: Center(
         child: ElevatedButton(
-        onPressed: fetch,
+        onPressed: () async {
+          await fetch();
+          print(stores.length);
+        },
         child: Text('테스트'),
         ),
       ),
