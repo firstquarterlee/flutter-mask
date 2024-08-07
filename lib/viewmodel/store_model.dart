6da5f9c6fc02bf08ce8ae8349c2d3b4639d6330b
이상현 <firstquarterlee@gmail.com>
2024-08-07 16:53:44 +0900

    Refactor: 로딩 처리와 View 분리

--- lib/viewmodel/store_model.dart
+++ lib/viewmodel/store_model.dart
@@ -3,6 +3,7 @@ import 'package:flutter_mask/model/store.dart';
 import 'package:flutter_mask/repository/store_repository.dart';
 
 class StoreModel with ChangeNotifier {
+  var isLoading = false;
   List<Store> stores = [];
 
   final _storeRepository = StoreRepository();
@@ -12,7 +13,11 @@ class StoreModel with ChangeNotifier {
   }
 
   Future fetch() async {
+    isLoading = true;
+    notifyListeners();
+
     stores = await _storeRepository.fetch();
+    isLoading = false;
     notifyListeners();
   }
 }
